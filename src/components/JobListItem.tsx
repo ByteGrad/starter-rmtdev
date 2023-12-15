3fe6b2dcac90d27e3703fc477d08054cb8403207
ByteGrad <bytegrad@gmail.com>
2023-12-15 11:08:30 +0100

    save

--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -9,20 +9,6 @@ export default function JobListItem() {
         <div className="job-item__middle">
           <h3 className="third-heading">Front End React Engineer</h3>
           <p className="job-item__company">9th Tech</p>
-          <div className="job-item__extras">
-            <p className="job-item__extra">
-              <i className="fa-solid fa-clock job-item__extra-icon"></i>
-              Full-Time
-            </p>
-            <p className="job-item__extra">
-              <i className="fa-solid fa-money-bill job-item__extra-icon"></i>
-              $105,000+
-            </p>
-            <p className="job-item__extra">
-              <i className="fa-solid fa-location-dot job-item__extra-icon"></i>{" "}
-              Global
-            </p>
-          </div>
         </div>
 
         <div className="job-item__right">
